fcd61587ac425654023162211b5a1890b35ccd39
Chasity Davis <111513093+chasityhdavis@users.noreply.github.com>
2025-02-27 14:24:37 -0500

    changed clear button and page description

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Reading Recommendation Consultant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by C.D. @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
 export const PAGE_TITLE: string = `Reading Recommendation Consultant`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s Reading Recommendation Consultant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
