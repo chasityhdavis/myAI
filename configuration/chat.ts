26aeb9d5487a1ca4f0f87fcee6ef129bb8bfd721
Chasity Davis <111513093+chasityhdavis@users.noreply.github.com>
2025-03-05 13:40:35 -0500

    Changed initial message

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}' Reading Recommendation Consultant.`;
+export const INITIAL_MESSAGE: string = `Hi! I'm ${AI_NAME}, your personal reading recommendation consultant. 
+Tell me a little about the type of literature you enjoy—your favorite genres, books, or tropes—and I'll help you find your next favorite read!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
